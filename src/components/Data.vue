b73371f822c2e52bc0a517ab04e1568897f9a933
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-22 18:48:43 +0400

    replacing data with ref

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,10 +1,24 @@
 <template>
     <h2>Options - {{o_firstName}}</h2>
+    <h2>Composition - {{c_firstName }}</h2>
+    <h2>Compsoition - {{greet}}</h2>
 </template>
 
 <script>
+import {ref} from "vue";
     export default {
         name: 'DataA',
+        setup(){
+          const c_firstName = ref('Clark');
+          c_firstName.value = 'Diana';
+          const greet = `Hello ${c_firstName.value}`;
+          console.log(c_firstName);
+
+          return {
+            c_firstName,
+            greet
+          }
+        },
         data(){
             return {
                 o_firstName: 'Bruce',
