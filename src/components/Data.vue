59aefa3b18893d5715b99967ab11ca0b0b15ae0f
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-22 18:41:07 +0400

    project setup

---
+++ src/components/Data.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>Options - {{o_firstName}}</h2>
+</template>
+
+<script>
+    export default {
+        name: 'DataA',
+        data(){
+            return {
+                o_firstName: 'Bruce',
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
