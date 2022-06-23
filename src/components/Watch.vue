c6ea05a0679b980661adc58d867f974e13769674
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-23 14:59:30 +0400

    replacing watchers andh reactive

--- src/components/Watch.vue
+++ src/components/Watch.vue
@@ -5,14 +5,60 @@
 
        <input type="text" placeholder="First Name" v-model="firstName">
        <input type="text" placeholder="Last Name" v-model="lastName">
+
+       <input type="text" placeholder="Reactive First Name" v-model="fName">
+       <input type="text" placeholder="Reactive Last Name" v-model="lName">
+       <input type="text" placeholder="Reactive Name" v-model="options.name">
+       <input type="text" placeholder="Reactive Hero Name" v-model="options.heroName">
     </div>
 </template>
 
 <script>
-import {ref, watch} from "vue";
+import {ref, watch, reactive, toRefs} from "vue";
     export default {
         name: 'WatchA',
         setup(){
+
+          const state = reactive({
+            fName: '',
+            lName: '',
+            options: {
+                name: '',
+                heroName: ''
+            }
+          })
+
+        //watching multiple proties at a time
+        //   watch(() => {
+        //     return {...state}
+        //   }, (newValue, oldValue)=> {
+        //     console.log('fName old Value', oldValue.fName);
+        //     console.log('fName new Value', newValue.fName);
+        //     console.log('lName old Value', oldValue.lName);
+        //     console.log('lName new Value', newValue.lName);
+        //   })
+
+       //watching invidual property
+        watch(()=> state.fName, (newValue,oldValue)=> {
+               console.log('fName old Value', oldValue);
+               console.log('fName new Value', newValue);
+        })
+
+        //watching invidual property's current value
+        watch(()=> state.lName, (value)=> {
+               console.log('lname value', value);
+        })
+
+        //watching deeply nested properties
+        watch(()=> state.options, (newValue,oldValue)=> {
+               console.log('options old Value', oldValue);
+               console.log('options new Value', newValue);
+        },
+        {
+            deep: true
+        }
+        )
+
           const firstName = ref('');
           const lastName = ref('Wayne');
           
@@ -29,7 +75,8 @@ import {ref, watch} from "vue";
 
           return {
             firstName,
-            lastName
+            lastName,
+            ...toRefs(state)
           }
         },
         data(){
